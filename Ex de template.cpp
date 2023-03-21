f8b4ddfb80705bfac96db965eb81d0f5c758e985
GustaHSR <126812739+GustaHSR@users.noreply.github.com>
2023-03-20 21:44:27 -0300

    Update Ex de template.cpp

--- Ex de template.cpp
+++ Ex de template.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+using namespace std;
 #define endl '\n'
 #define ff first
 #define ss second
@@ -9,12 +10,12 @@
 #define all(x) x.begin(), x.end()
 #define rall(x) x.rbegin(), x.rend()
 #define ld long double
-using namespace std;
-
 using ll = long long;
-using ii = pair<int, int>;
-template<typename MATHEUS>
-void imprime(MATHEUS *v, int n) {
+
+template<typename MATHEUS,typename SEXO,typename MUITO>
+void imprime(MATHEUS *v, MUITO n, SEXO a) {
+    a = "Eu só quis colocar esse a  para usar o typename SEXO.";
+    cout << a << endl << endl;
     cout << "vetor[";
     for (int i = 0; i < n;i++) {
         cout << fixed<<setprecision(2) << v[i] << " ";
@@ -25,11 +26,12 @@ void imprime(MATHEUS *v, int n) {
 int main() {
     int n; cin >> n;
 	int v[n];
+    string a;
     rpara(v,n);
-    imprime(v,n);
+    imprime(v,n,a);
     double v1[n];
     for (int i = 0; i < n;i++) {
          v1[i] = (double)rand()*100.0/(double)RAND_MAX;
     }
-    imprime(v1,n);
+    imprime(v1,n,a);
 }
