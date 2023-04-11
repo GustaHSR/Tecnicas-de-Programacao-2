4eb8eaa4e1494b25fa3a2de8a7173c2a0f1caa21
GustaHSR <126812739+GustaHSR@users.noreply.github.com>
2023-04-11 09:07:30 -0300

    Create SobrecargaDeVetores.cpp

---
+++ SobrecargaDeVetores.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#include "Header.h"
+
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define pb push_back
+#define f first
+#define s second
+#define endl '\n'
+
+int main () {
+	Vetor A(3);
+	cout << "Tamanho de A: " << A.getTam() << endl;
+	cout << "Vetor A: " << A << endl;
+	cout << "Preencha o vetor A: ";
+	cin >> A;
+	cout << "Vetor A: " << A << endl;
+
+	Vetor B;
+	cout << "Tamanho de B: " << B.getTam() << endl;
+	cout << "Vetor B: " << B << endl;
+
+	
+}
